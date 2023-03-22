b33c20564fa1513ebc4438f277b32ddfd20997e9
Joseph Villarreal Lopez <lapeyus@gmail.com>
2023-03-22 17:14:35 -0600

    Add code to analyze syntax in a string

--- main.py
+++ main.py
@@ -1,63 +1,272 @@
-# Import the required module
-import pyttsx3
- 
-# Create a string
-string = "Lorem Ipsum is simply dummy text " \
-    + "of the printing and typesetting industry."
- 
-# Initialize the Pyttsx3 engine
-engine = pyttsx3.init()
- 
-# We can use file extension as mp3 and wav, both will work
-engine.save_to_file(string, 'speech.mp3')
- 
-# Wait until above command is not finished.
-engine.runAndWait()
+import os
+from google.cloud import language_v1
+import six
 
+# Set the environment variable for the JSON key
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './key.json'
 
-# # Open the PDF file in read-binary mode using a context manager
-# with open('demo.pdf', 'rb') as pdf_file:
-#     # Create a PDF reader object
-#     pdf_reader = PdfReader(pdf_file)
+def sample_analyze_entities(text_content):
+    """
+    Analyzing Entities in a String
 
-#     # Get the total number of pages in the PDF file
-#     num_pages = len(pdf_reader.pages)
+    Args:
+      text_content The text content to analyze
+    """
 
-#     # Create a text variable to store the content of the PDF file
-#     text = ''
+    client = language_v1.LanguageServiceClient()
 
-#     # Iterate over each page in the PDF file
-#     for page in range(num_pages):
-#         # Get the page object
-#         pdf_page = pdf_reader.pages[page]
+    # text_content = 'California is a state.'
 
-#         # Extract the text from the page
-#         page_text = pdf_page.extract_text()
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
 
-#         # Add the text to the text variable
-#         text += page_text
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
 
-# # Create a text-to-speech engine object
-# engine = pyttsx3.init()
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
 
-# # Convert the text to speech
-# engine.say(text)
-# engine.runAndWait()
+    response = client.analyze_entities(
+        request={"document": document, "encoding_type": encoding_type}
+    )
 
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        print("Representative name for the entity: {}".format(entity.name))
 
+        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+        print("Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
 
+        # Get the salience score associated with the entity in the [0, 1.0] range
+        print("Salience score: {}".format(entity.salience))
 
-# import pyttsx3,PyPDF2
-# speacker = pyttsx3.init()
+        # Loop over the metadata associated with entity. For many known entities,
+        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+        # Some entity types may have additional metadata, e.g. ADDRESS entities
+        # may have metadata for the address street_name, postal_code, et al.
+        for metadata_name, metadata_value in entity.metadata.items():
+            print("{}: {}".format(metadata_name, metadata_value))
 
-# texts = ["This is an example of text-to-speech conversion", "Hello World"]
-# speacker.save_to_file (texts, "d.mp3")
-# speacker.say('hello')
-# # pdfreader = PyPDF2.PdfReader(open('demo.pdf','rb'))
-# # for page_num in range (len(pdfreader.pages)):
-# #     text  = pdfreader.pages[page_num].extract_text()
-# #     clean_text = text.strip().replace('\n', ' ')
-# #     print(clean_text)
-# # speacker.save_to_file(clean_text, 'audio.mp3')
-# # speacker.runAndWait()
-# # speacker.stop()
+        # Loop over the mentions of this entity in the input document.
+        # The API currently supports proper noun mentions.
+        for mention in entity.mentions:
+            print("Mention text: {}".format(mention.text.content))
+
+            # Get the mention type, e.g. PROPER for proper noun
+            print(
+                "Mention type: {}".format(
+                    language_v1.EntityMention.Type(mention.type_).name
+                )
+            )
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print("Language of the text: {}".format(response.language))
+
+def sample_analyze_entity_sentiment(text_content):
+    """
+    Analyzing Entity Sentiment in a String
+
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'Grapes are good. Bananas are bad.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.types.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_entity_sentiment(
+        request={"document": document, "encoding_type": encoding_type}
+    )
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        print("Representative name for the entity: {}".format(entity.name))
+        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+        print("Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
+        # Get the salience score associated with the entity in the [0, 1.0] range
+        print("Salience score: {}".format(entity.salience))
+        # Get the aggregate sentiment expressed for this entity in the provided document.
+        sentiment = entity.sentiment
+        print("Entity sentiment score: {}".format(sentiment.score))
+        print("Entity sentiment magnitude: {}".format(sentiment.magnitude))
+        # Loop over the metadata associated with entity. For many known entities,
+        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+        # Some entity types may have additional metadata, e.g. ADDRESS entities
+        # may have metadata for the address street_name, postal_code, et al.
+        for metadata_name, metadata_value in entity.metadata.items():
+            print("{} = {}".format(metadata_name, metadata_value))
+
+        # Loop over the mentions of this entity in the input document.
+        # The API currently supports proper noun mentions.
+        for mention in entity.mentions:
+            print("Mention text: {}".format(mention.text.content))
+            # Get the mention type, e.g. PROPER for proper noun
+            print(
+                "Mention type: {}".format(
+                    language_v1.EntityMention.Type(mention.type_).name
+                )
+            )
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print("Language of the text: {}".format(response.language))
+
+def sample_classify_text(text_content):
+    """
+    Classifying Content in a String
+
+    Args:
+      text_content The text content to analyze.
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = "That actor on TV makes movies in Hollywood and also stars in a variety of popular new TV shows."
+    print(text_content)
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    content_categories_version = (
+        language_v1.ClassificationModelOptions.V2Model.ContentCategoriesVersion.V2
+    )
+    response = client.classify_text(
+        request={
+            "document": document,
+            "classification_model_options": {
+                "v2_model": {"content_categories_version": content_categories_version}
+            },
+        }
+    )
+    print(response)
+    # Loop through classified categories returned from the API
+    for category in response.categories:
+        # Get the name of the category representing the document.
+        # See the predefined taxonomy of categories:
+        # https://cloud.google.com/natural-language/docs/categories
+        print("Category name: {}".format(category.name))
+        # Get the confidence. Number representing how certain the classifier
+        # is that this category represents the provided text.
+        print("Confidence: {}".format(category.confidence))
+
+def sample_analyze_sentiment(content):
+
+    client = language_v1.LanguageServiceClient()
+
+    if isinstance(content, six.binary_type):
+        content = content.decode("utf-8")
+
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+    document = {"type_": type_, "content": content}
+
+    response = client.analyze_sentiment(request={"document": document})
+    sentiment = response.document_sentiment
+    print("Score: {}".format(sentiment.score))
+    print("Magnitude: {}".format(sentiment.magnitude))
+
+def sample_analyze_syntax(text_content):
+    """
+    Analyzing Syntax in a String
+
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'This is a short sentence.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_syntax(
+        request={"document": document, "encoding_type": encoding_type}
+    )
+    # Loop through tokens returned from the API
+    for token in response.tokens:
+        # Get the text content of this token. Usually a word or punctuation.
+        text = token.text
+        print("Token text: {}".format(text.content))
+        print(
+            "Location of this token in overall document: {}".format(text.begin_offset)
+        )
+        # Get the part of speech information for this token.
+        # Part of speech is defined in:
+        # http://www.lrec-conf.org/proceedings/lrec2012/pdf/274_Paper.pdf
+        part_of_speech = token.part_of_speech
+        # Get the tag, e.g. NOUN, ADJ for Adjective, et al.
+        print(
+            "Part of Speech tag: {}".format(
+                language_v1.PartOfSpeech.Tag(part_of_speech.tag).name
+            )
+        )
+        # Get the voice, e.g. ACTIVE or PASSIVE
+        print(
+            "Voice: {}".format(
+                language_v1.PartOfSpeech.Voice(part_of_speech.voice).name
+            )
+        )
+        # Get the tense, e.g. PAST, FUTURE, PRESENT, et al.
+        print(
+            "Tense: {}".format(
+                language_v1.PartOfSpeech.Tense(part_of_speech.tense).name
+            )
+        )
+        # See API reference for additional Part of Speech information available
+        # Get the lemma of the token. Wikipedia lemma description
+        # https://en.wikipedia.org/wiki/Lemma_(morphology)
+        print("Lemma: {}".format(token.lemma))
+        # Get the dependency tree parse information for this token.
+        # For more information on dependency labels:
+        # http://www.aclweb.org/anthology/P13-2017
+        dependency_edge = token.dependency_edge
+        print("Head token index: {}".format(dependency_edge.head_token_index))
+        print(
+            "Label: {}".format(
+                language_v1.DependencyEdge.Label(dependency_edge.label).name
+            )
+        )
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print("Language of the text: {}".format(response.language))
+
+if __name__ == "__main__":
+    text_content = "That actor on TV makes movies in Hollywood and also stars in a variety of popular new TV shows."
+    # sample_classify_text(text_content)
+    # sample_analyze_entities(text_content)
+    # sample_analyze_entity_sentiment(text_content)
+    # sample_analyze_sentiment(text_content)
+    sample_analyze_syntax(text_content)
